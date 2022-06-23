fade539155490fcfe50b8b9e9cb9ee901dd9eec8
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-06-23 14:57:20 -0700

    Better error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,3 +1,19 @@
+<script context="module" lang="ts">
+  /** @type {import('@sveltejs/kit').Load} */
+  export function load({ error, status }: { error: Error; status: number }) {
+    return {
+      props: {
+        title: `${status}: ${error?.message}`
+      }
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let title: string;
+</script>
+
 <main>
+  <h1>{title}</h1>
   <a href="/">Go home</a>
 </main>
