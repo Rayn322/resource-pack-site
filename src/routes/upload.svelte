5a1e0a900649838d8af78c1bc3674237df56e17d
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-07-11 15:50:25 -0700

    Make pack details editable

--- src/routes/upload.svelte
+++ src/routes/upload.svelte
@@ -9,15 +9,17 @@
   let imageBase64: string;
 
   // TODO: detect when submitted file isn't a resource pack
-  // TODO: make details editable
-  async function onSubmit(event: SubmitEvent) {
+  async function onPackSubmit() {
     if (files && files.length > 0) {
-      console.log('submitted');
       ({ name, description, imageBase64 } = await processZip(files[0]));
       confirmed = true;
-    } else {
-      console.log('no files');
     }
+  }
+
+  // TODO: actually upload
+  function onPackUpload() {
+    console.log(name);
+    console.log(description);
   }
 
   function onFileSelect() {
@@ -33,18 +35,30 @@
 <main>
   <div>
     {#if confirmed}
-      <div class="card">
+      <form class="card" on:submit|preventDefault={onPackUpload}>
         <h2 class="title">Confirm Details</h2>
-        <p>Name: {name}</p>
-        <p>Description: {description}</p>
+        <label for="name" class="text-label">Name</label>
+        <input type="text" name="name" id="name" class="text-input" required bind:value={name} />
+
+        <label for="description" class="text-label">Description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          class="text-input"
+          required
+          bind:value={description}
+        />
+
         <img src={imageBase64} alt={name} />
-        <button class="submit">Upload</button>
-        <button class="button" on:click={() => (confirmed = false)}> Cancel </button>
-      </div>
+
+        <input class="submit upload" type="submit" value="Upload" />
+        <button class="cancel" on:click={() => (confirmed = false)}>Cancel</button>
+      </form>
     {:else}
-      <form class="card" on:submit|preventDefault={(event) => onSubmit(event)}>
+      <form class="card" on:submit|preventDefault={onPackSubmit}>
         <h2 class="title">Upload a Resource Pack</h2>
-        <label for="pack">{text}</label>
+        <label for="pack" class="pack-label">{text}</label>
         <input
           bind:files
           on:change={onFileSelect}
@@ -82,9 +96,18 @@
     text-align: center;
   }
 
-  p {
+  .text-label {
+    margin-bottom: 0.25em;
+  }
+
+  .text-input {
+    font-size: inherit;
     margin: 0;
-    margin-bottom: 1em;
+    margin-bottom: 0.75em;
+    padding: 0.25em;
+    border-radius: 4px;
+    border: 2px solid var(--font-color);
+    background-color: transparent;
   }
 
   img {
@@ -95,7 +118,7 @@
     display: none;
   }
 
-  label {
+  .pack-label {
     font-size: 1em;
     cursor: pointer;
     padding: 0.3em 0.4em;
@@ -107,13 +130,16 @@
     hyphens: auto;
   }
 
-  button {
+  .cancel {
     font-size: 1em;
     cursor: pointer;
     padding: 0.3em 0.4em;
     border: 2px solid var(--font-color);
     border-radius: 8px;
     background-color: transparent;
+  }
+
+  .upload {
     margin-bottom: 1em;
   }
 
