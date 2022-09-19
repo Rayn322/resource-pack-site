f94be6db9bda767b850ab2b1ebc914ae4f514032
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-09-18 20:18:46 -0700

    Fix initializing Firebase

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -54,3 +54,5 @@ export function signOut() {
 	auth.signOut();
 	goto('/');
 }
+
+initFirebase();
--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-import { initFirebase } from '$lib/firebase/firebase';
-import type { LayoutLoad } from './$types';
-
-export function load(): LayoutLoad {
-	initFirebase();
-	return {};
-}
