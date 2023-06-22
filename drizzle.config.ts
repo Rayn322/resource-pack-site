60f66e964a21260668781bb03adefb1fba578330
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-06-22 13:15:59 -0700

    Update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,11 @@ import type { Config } from 'drizzle-kit';
 // t3 env doesn't seem to work here
 import 'dotenv/config';
 
-const config = {
+export default {
 	out: './drizzle',
 	schema: './src/db/schema.ts',
-	connectionString: process.env.DATABASE_URL,
+	driver: 'mysql2',
+	dbCredentials: {
+		connectionString: process.env.DATABASE_URL ?? '',
+	},
 } satisfies Config;
-
-export default config;
