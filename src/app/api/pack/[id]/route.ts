08b742ebae7bac32b1d723bd7185187975af97ee
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-06-26 19:31:18 -0700

    Api for getting packs by id

---
+++ src/app/api/pack/[id]/route.ts
@@ -0,0 +1,28 @@
+import { db } from '@/db/db';
+import { packs } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+	request: Request,
+	context: { params: { id: string } }
+) {
+	const id = parseInt(context.params.id);
+
+	if (isNaN(id)) {
+		return NextResponse.json(
+			{ error: 'Error parsing pack id' },
+			{ status: 400 }
+		);
+	}
+
+	const pack = await db.query.packs.findFirst({
+		where: eq(packs.id, id),
+	});
+
+	if (!pack) {
+		return NextResponse.json({ error: 'Pack not found' }, { status: 404 });
+	}
+
+	return NextResponse.json(pack);
+}
