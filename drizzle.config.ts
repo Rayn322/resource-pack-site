df1b93b852b3ffc82334297b3138f8e3994b6913
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-10-16 13:42:23 -0700

    Fix drizzle-kit env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,10 @@
 import type { Config } from 'drizzle-kit';
-// t3 env doesn't seem to work here
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' }); // file name when using vercel env pull
+import { env } from '@/env.mjs';
 
 export default {
-	out: './drizzle',
 	schema: './src/db/schema.ts',
 	driver: 'mysql2',
 	dbCredentials: {
-		connectionString: process.env.DATABASE_URL ?? '',
+		connectionString: env.DATABASE_URL,
 	},
 } satisfies Config;
