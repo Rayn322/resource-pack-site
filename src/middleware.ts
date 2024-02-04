0778f50aace3940413fc024a50cbe8a8045d9797
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-02-03 18:08:46 -0800

    Add packs/[id] to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,14 @@ import { authMiddleware } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 export default authMiddleware({
-	publicRoutes: ['/', '/packs', '/sign-in', '/sign-up', '/api/(.*)'],
+	publicRoutes: [
+		'/',
+		'/packs',
+		'/packs/:id',
+		'/sign-in',
+		'/sign-up',
+		'/api/(.*)',
+	],
 	afterAuth: (auth, req) => {
 		if (!auth.sessionId && !auth.isPublicRoute) {
 			const url = req.nextUrl.clone();
