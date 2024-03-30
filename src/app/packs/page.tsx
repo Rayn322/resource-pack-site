26049b9dcf98c78fbcb216a0d4abff0e142c4bac
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-03-30 13:07:14 -0700

    Check if there are any packs

--- src/app/packs/page.tsx
+++ src/app/packs/page.tsx
@@ -9,9 +9,11 @@ export default async function Packs() {
 		<>
 			<h1 className="text-3xl">Resource Packs</h1>
 			<div className="mx-32 flex flex-wrap justify-center gap-4">
-				{stuff.map((pack) => (
-					<Card key={pack.id} pack={pack} />
-				))}
+				{stuff.length > 0 ? (
+					stuff.map((pack) => <Card key={pack.id} pack={pack} />)
+				) : (
+					<p>No packs found</p>
+				)}
 			</div>
 		</>
 	);
